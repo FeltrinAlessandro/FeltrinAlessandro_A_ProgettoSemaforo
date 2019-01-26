42c2b0f4e16240882a662f19aed6d203cc29c8ea
Unknown <alessandro.feltrin@barsanti.edu.it>
2019-01-26 08:51:54 +0100

    25%

--- ProgettoSemaforo/ProgettoSemaforo.ino
+++ ProgettoSemaforo/ProgettoSemaforo.ino
@@ -18,12 +18,19 @@ void lampeggia(int led,int n)
     for(int i=0,i<n,i++)
   {
     digitalWrite(led,LOW);
+    delay 333;
     digitalWrite(led,HIGH);
   }
     digitalWrite(led,LOW);
   }
 
 void loop() {
+  digitalWrite(verde1,HIGH)
+  digitalWrite(giallo2,HIGH)
+  delay 3000;
+  digitalWrite(giallo2,LOW)
+  digitalWrite(rosso2,HIGH)
+  delay 7000;
   
 
 }
