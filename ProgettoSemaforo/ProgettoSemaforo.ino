385af4ac329da8f9cb5ee9dee9c231c7722572a7
AlessandroFeltrin <alessandro.feltrin@barsanti.edu.it>
2019-02-01 18:42:53 +0100

    40%

--- ProgettoSemaforo/ProgettoSemaforo.ino
+++ ProgettoSemaforo/ProgettoSemaforo.ino
@@ -5,6 +5,12 @@ int verde1 = 7;
 int verde2 = 8;
 int giallo1 = 12;
 int giallo2 = 13;
+int durataSemaforo;
+int durataGiallo;
+int lampeggiVerde;
+int intervalloLampeggi;
+int durataSoloRosso;
+int durataSoloVerde;
 pinMode(rosso1,OUTPUT);
 pinMode(rosso2,OUTPUT);
 pinMode(verde1,OUTPUT);
@@ -13,15 +19,22 @@ pinMode(giallo1,OUTPUT);
 pinMode(giallo2,OUTPUT);
 }
 
-void lampeggia(int led,int n,int t)
+void lampeggiaVerde()
   {
-    for(int i=0,i<n,i++)
+    for(int i=0,i<lampeggiVerde,i++)
   {
-    digitalWrite(led,LOW);
-    delay t;
-    digitalWrite(led,HIGH);
+    digitalWrite(verde1,LOW);
+    delay intervalloLampeggi;
+    digitalWrite(verde1,HIGH);
   }
     digitalWrite(led,LOW);
+  }
+  void metodo1()
+  {
+    digitalWrite(rosso2,HIGH);
+    digitalWrite(verde1,HIGH);
+    delay durataSoloVerde;
+    lampeggiaVerde();
   }
 
 void loop() {
